2e044a06cd416149e164786adbff1333f4ddbbb4
Suaden.Rowland <suadenrowland@gmail.com>
2023-09-13 12:10:11 -0700

    added armour pen hp and hp values

--- arrays.js
+++ arrays.js
@@ -4,13 +4,31 @@
 */
 
 let jonSnowAttack = 75
-let jamieLannisterAttack = 75
+let jamieLannisterAttack = 100
 
 if ( jonSnowAttack > jamieLannisterAttack) {
-    console.log('Jon Snow has better attack than Jamie Lannister')
+    console.log('Jon Snow has higher attack than Jamie Lannister')
 }
 else if ( jamieLannisterAttack > jonSnowAttack) { 
-    console.log('Jamie Lannister has better attack than Jon Snow')
+    console.log('Jamie Lannister has higher attack than Jon Snow')
 } else {
     console.log("Jamie is still better tbh")
+}
+
+let jonSnowHealth = 100
+let jamieLannisterHealth = 125
+let jonSnowArmour = 25
+let jamieLannisterArmour = 50
+let jonSnowArmourPenetration = 10
+let jamieLannisterArmourPenetration = 15
+
+if ( jonSnowHealth + jonSnowArmour <= jamieLannisterAttack + jamieLannisterArmourPenetration) {
+    console.log('Jon Snow has been slain by Jamie Lannister')
+} 
+else if ( jonSnowAttack + jonSnowArmourPenetration >= jamieLannisterHealth + jamieLannisterArmour) {
+    console.log(`Jamie Lannister has been slain by Jon Snow`)
+}
+else {
+    jonSnowHealth = jonSnowHealth + jonSnowArmour - jamieLannisterAttack - jamieLannisterArmourPenetration
+    console.log (`Jon Snow's current health is ${jonSnowHealth}`)
 }
