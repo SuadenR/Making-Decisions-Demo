1dfa75ad50cecdfc9e2f38206f0510d26b58a9ab
Suaden.Rowland <suadenrowland@gmail.com>
2023-09-13 11:38:55 -0700

    initial commit

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/*
+    In this file, we'll be continuing our story
+    with Jon being allowed to flee his fight with Jamie.
+*/
+
+let jonSnowAttack = 75
+let jamieLannisterAttack = 75
+
+if ( jonSnowAttack > jamieLannisterAttack) {
+    console.log('Jon Snow has better attack than Jamie Lannister')
+}
+else if ( jamieLannisterAttack > jonSnowAttack) { 
+    console.log('Jamie Lannister has better attack than Jon Snow')
+} else {
+    console.log("Jamie is still better tbh")
+}
